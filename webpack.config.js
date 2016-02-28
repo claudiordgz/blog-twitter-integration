dea4c02c1d3af4082cf9a72abddc481a0c0ffea2
claudiordgz <claudio.rdgz@gmail.com>
2016-02-27 23:19:17 -0500

    moved query to babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -19,16 +19,9 @@ module.exports = {
             {
                 loader: "babel-loader",
                 // Skip any files outside of your project's `src` directory
-                include: [
-                    path.resolve(__dirname, "src")
-                ],
+                exclude: /(node_modules|bower_components)/,
                 // Only run `.js` and `.jsx` files through Babel
-                test: /\.js?$/,
-                // Options to configure babel with
-                query: {
-                    plugins: ['transform-runtime'],
-                    presets: ['es2015', 'stage-0']
-                }
+                test: /\.js?$/
             }
         ],
         postLoaders: [
