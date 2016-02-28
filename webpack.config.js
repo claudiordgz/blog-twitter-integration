73196438f21f557007c0b0456826d98efe67f846
claudiordgz <claudio.rdgz@gmail.com>
2016-02-27 22:50:39 -0500

    feat: babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var webpack = require('webpack');
+var webpack = require('webpack'),
+    path = require('path');
 
 
 // webpack.config.js
@@ -15,6 +16,20 @@ module.exports = {
     },
     module: {
         loaders: [
+            {
+                loader: "babel-loader",
+                // Skip any files outside of your project's `src` directory
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+                // Only run `.js` and `.jsx` files through Babel
+                test: /\.js?$/,
+                // Options to configure babel with
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015', 'stage-0']
+                }
+            }
         ],
         postLoaders: [
             {
